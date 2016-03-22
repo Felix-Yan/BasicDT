77056220c64b4d11621ff42ccb71ad5718839779
Felix Yan <286589258@qq.com>
2016-03-22 14:01:49 +1300

    Finishing building the DT

--- imp/DTLearning.java
+++ imp/DTLearning.java
@@ -9,11 +9,12 @@ import java.util.List;
  *
  */
 public class DTLearning {
-	Helper trainingSet;
-	Helper testingSet;
-	List<String> categoryNames;
-	List<String> attNames;
-	Node root;//root node of the decision tree
+	private Helper trainingSet;
+	private Helper testingSet;
+	private List<String> categoryNames;
+	private List<String> attNames;
+	private Node root;//root node of the decision tree
+	public static final int DUMMY_INDEX = 999;//only to satisfy the node constructor
 
 	/**
 	 * This constructs a learned decision tree from helpers.
@@ -25,6 +26,34 @@ public class DTLearning {
 		testingSet = test;
 		categoryNames = trainingSet.getCategoryNames();
 		attNames = trainingSet.getAttNames();
+		root = BuildTree(trainingSet.getAllInstances(), attNames);
+	}
+
+	/**
+	 * This tests the prediction accuracy of the decision tree against a test set
+	 */
+	private void testAccuracy(){
+		List<Helper.Instance> testInstances = testingSet.getAllInstances();
+
+	}
+
+	/**
+	 * This tests the correctness of a single instance prediction
+	 * @return
+	 */
+	private boolean testInstance(Helper.Instance instance){
+		Node current = root;
+		while(current.attIndex != DUMMY_INDEX){
+			if(instance.getAtt(current.attIndex)){
+				current = root.left;
+			}else{
+				current = root.right;
+			}
+		}
+		if(current.attribute.equals(categoryNames.get(instance.getCategory()))){
+			return true;//true if the predicted class name is the same as the instance class
+		}
+		return false;//false otherwise
 	}
 
 	/**
@@ -41,7 +70,7 @@ public class DTLearning {
 		if(isPure(instances)){
 			int category = instances.get(0).getCategory();
 			String name = categoryNames.get(category);
-			return new LeafNode(null,null,name,1);
+			return new LeafNode(null,null,name,DUMMY_INDEX,1);
 		}
 
 		if(attributes.isEmpty()){
@@ -49,12 +78,68 @@ public class DTLearning {
 			return findMostProbableNode(instances);
 		}
 		else{
+			int bestAttribute = findBestAttribute(instances, attributes);
+			List<Helper.Instance> bestInstsTrue = findTrueList(instances, bestAttribute);
+			//now instances represents bestInstsFalse.
+			instances.removeAll(bestInstsTrue);
+			//remove the current best attributes by setting it to null. Size does not change.
+			attributes.set(bestAttribute, null);
+			Node left = BuildTree(bestInstsTrue, attributes);
+			Node right = BuildTree(instances, attributes);
+			return new Node(left, right, attNames.get(bestAttribute), bestAttribute);
 		}
-		return null;//to compile
 	}
 
-	private double computePurity(List<Helper.Instance> instances ){
-		return 0; //to compile
+	/**
+	 * This finds out the best attribute, which has the smallest impurity.
+	 * @param instances
+	 * @return
+	 */
+	private int findBestAttribute(List<Helper.Instance> instances, List<String> attributes){
+		double minImpurity = Double.MAX_VALUE;
+		int bestAttribute = 0;
+		for(int i=0; i<attributes.size(); i++){
+			if(attributes.get(i) == null){
+				continue;//The attribute has been removed in previous nodes. Compute next attribute.
+			}
+			double impurity = computeImpurity(instances, i);
+			if(impurity < minImpurity){
+				minImpurity = impurity;
+				bestAttribute = i;
+			}
+		}
+		return bestAttribute;
+	}
+
+	/**
+	 * This computes the impurity of given instances by using formula A/(A+B) * B/(A+B), where A and B are two sets of
+	 * the instances with opposite value of the attribute.
+	 * @param instances
+	 * @param index The index of attribute in attNames.
+	 * @return
+	 */
+	private double computeImpurity(List<Helper.Instance> instances, int index ){
+		List<Helper.Instance> trueList = findTrueList(instances, index);
+		int A = trueList.size();
+		int B = instances.size() - trueList.size();
+		double impurity = A*1.0/(A+B)*B/(A+B);
+		return impurity;
+	}
+
+	/**
+	 * This finds out the list of instances that has the true value of the given attribute
+	 * @param instances
+	 * @param index
+	 * @return
+	 */
+	private List<Helper.Instance> findTrueList(List<Helper.Instance> instances, int index){
+		List<Helper.Instance> trueList = new ArrayList<Helper.Instance>();
+		for(Helper.Instance i: instances){
+			if(i.getAtt(index)){
+				trueList.add(i);
+			}
+		}
+		return trueList;
 	}
 
 	/**
@@ -71,7 +156,7 @@ public class DTLearning {
 		int maxIndex = findMaxIndex(counts);
 		double probability = counts[maxIndex]*1.0/instances.size();
 		String name = categoryNames.get(maxIndex);
-		return new LeafNode(null,null,name,probability);
+		return new LeafNode(null,null,name,DUMMY_INDEX, probability);
 	}
 
 	/**
@@ -114,7 +199,10 @@ public class DTLearning {
 	 * @param args
 	 */
 	public static void main(String[] args){
-		if(args.length<2) return;
+		if(args.length<2){
+			System.out.println("You need to give two file names for training and testing");
+			return;
+		}
 		Helper training = new Helper(args[0]);
 		Helper testing = new Helper(args[1]);
 		new DTLearning(training, testing);
@@ -129,15 +217,17 @@ public class DTLearning {
 		public final Node left;
 		public final Node right;
 		public final String attribute;
+		public final int attIndex;
 		/**
 		 * @param left Left child Node.
 		 * @param right Right child Node.
 		 * @param attribute The attribute evaluated in this node.
 		 */
-		public Node(Node left, Node right, String attribute) {
+		public Node(Node left, Node right, String attribute, int index) {
 			this.left = left;
 			this.right = right;
 			this.attribute = attribute;
+			this.attIndex = index;
 		}
 	}
 
@@ -154,8 +244,8 @@ public class DTLearning {
 		 * @param attribute
 		 * @param p The probability of the most probable class.
 		 */
-		public LeafNode(Node left, Node right, String attribute, double p) {
-			super(left, right, attribute);
+		public LeafNode(Node left, Node right, String attribute, int index, double p) {
+			super(left, right, attribute, index);
 			probability = p;
 		}
 	}
